5e33561884893ea97efbc5e88d630907fe554429
Mauro Longano <mauro.longano@marconirovereto.it>
2025-01-01 17:19:35 +0100

    feat(config): add tailwind and shadcn/ui

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  integrations: [react()],
+  integrations: [ react(), tailwind( {
+          nesting: true,
+
+    })],
   vite: {
     define: {
       'process.env': process.env,
